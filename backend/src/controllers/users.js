158cdd78c489c63d511e7f19be0c3088f424ff76
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-18 11:15:44 -0300

    atualizando

---
+++ backend/src/controllers/users.js
@@ -0,0 +1,17 @@
+import UsersDataAccess from "../dataAccess/users.js";
+import { ok, serverError } from "../helpers/httpResponse.js";
+
+export default class UsersControllers {
+  constructor() {
+    this.dataAccess = new UsersDataAccess();
+  }
+  async getUsers() {
+    try {
+      const users = this.dataAccess.getUsers();
+
+      return ok(users);
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
