b937390e8ea0df62cb28eb2f01bcf502d15cbbe1
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-21 10:06:11 -0300

    atualizando

---
+++ backend/src/routes/plates.js
@@ -0,0 +1,31 @@
+import express from "express";
+import UsersControllers from "../controllers/users.js";
+
+const usersRouter = express.Router();
+
+const usersControllers = new UsersControllers();
+
+usersRouter.get("/", async (req, res) => {
+  const { success, statusCode, body } = await usersControllers.getUsers();
+
+  res.status(statusCode).send({ success, body, statusCode });
+});
+
+usersRouter.delete("/:id", async (req, res) => {
+  const userId = req.params.id;
+  const { success, statusCode, body } =
+    await usersControllers.deleteUser(userId);
+
+  res.status(statusCode).send({ success, statusCode, body });
+});
+
+usersRouter.put("/:id", async (req, res) => {
+  const { success, statusCode, body } = await usersControllers.updateUser(
+    req.params.id,
+    req.body,
+  );
+
+  res.status(statusCode).send({ success, statusCode, body });
+});
+
+export default usersRouter;
