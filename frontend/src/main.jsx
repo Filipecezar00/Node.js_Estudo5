81fcd33055d6604a7dbf55b04c91355475001870
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-10 10:42:04 -0300

    atualizando

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home copy/pages.jsx";
+import Cart from "./pages/cart/pages.jsx";
+import Profile from "./pages/profile/pages.jsx";
+import Auth from "./pages/auth/pages.jsx";
+import Plates from "./pages/plates/pages.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const pages = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/auth", element: <Auth /> },
+      { path: "/plates", element: <Plates /> },
+    ],
+  },
+]);
+
+creatRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
