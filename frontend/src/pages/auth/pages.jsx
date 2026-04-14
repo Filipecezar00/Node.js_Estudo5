826d13de2e0f07f0e41eb0c2bbad444cbf3c05a3
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-14 09:12:34 -0300

    atualizando

--- frontend/src/pages/auth/pages.jsx
+++ frontend/src/pages/auth/pages.jsx
@@ -2,11 +2,21 @@ import { useEffect, useState } from "react";
 import { TextField, Button } from "@mui/material";
 import style from "./page.module.css";
 import authServices from "../../services/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
   const [formType, setFormType] = useState("login");
   const [formData, setFormData] = useState({});
   const { login, signup, authLoading } = authServices();
+  const navigate = useNavigate();
+
+  const authData = JSON.parse(localStorage.getItem("auth"));
+
+  useEffect(() => {
+    if (authData) {
+      return navigate("/profile");
+    }
+  });
 
   const handleChangeFormType = () => {
     setFormData({});
--- frontend/src/pages/profile/pages.jsx
+++ frontend/src/pages/profile/pages.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Profile() {
+  const navigate = useNavigate();
+  const authData = JSON.parse(localStorage.getItem("auth"));
+
+  useEffect(() => {
+    if (!authData) {
+      return navigate("/auth");
+    }
+  }, []);
   return <h1>Profile</h1>;
 }
