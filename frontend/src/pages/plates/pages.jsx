320f7043ac8244cef6ee30960178dc80f1cf1495
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-29 16:37:24 -0300

    atualizando

--- frontend/src/pages/plates/pages.jsx
+++ frontend/src/pages/plates/pages.jsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+import usePlateServices from "../../services/plate";
+
 export default function Plates() {
+  const { getPlate, plateList, plateLoading, refetchPlate } =
+    usePlateServices();
+  useEffect(() => {
+    if (refetchPlate) {
+      getPlate();
+    }
+  }, [refetchPlate]);
+
+  if (plateLoading) {
+    return <Loading />;
+  }
+
+  console.log(plateList);
   return <h1>Plates</h1>;
 }
